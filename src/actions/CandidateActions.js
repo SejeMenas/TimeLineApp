9923440c936a22a8d80d4aa85da181d1ea38f37b
resendefreitas <guilherme.freitas@vtex.com.br>
2015-12-12 11:36:44 -0200

    Stores, fera!

--- src/actions/CandidateActions.js
+++ src/actions/CandidateActions.js
@@ -3,6 +3,10 @@ import flux from '../flux';
 // import Fetcher from 'utils/Fetcher';
 
 class CandidateActions {
-
+  fetchCandidate(name) {
+    const candidato = {};
+    candidato = name;
+    this.dispatch(candidato);
+  }
 }
 export default flux.createActions(CandidateActions);
