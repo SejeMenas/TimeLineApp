fbf3555258c1ba43662c7e294ee2fff3963a2011
resendefreitas <guilherme.freitas@vtex.com.br>
2015-12-12 14:00:00 -0200

    Creates Fetcher to access API

---
+++ src/fetcher.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+class Fetcher {
+  constructor() {
+    this.candidateSections = ['estado','nome','matricula'];
+    this.apiUrl = '/candidatos';
+  }
+
+  getPresident(name) {
+    return axios.get(`/${this.apiUrl}?estado=brasil&nome=${name}`);
+  }
+
+  getRjCandidate(name) {
+    return axios.get(`/${this.apiUrl}?estado=rj&nome=${name}`);
+  }
+
+  filterCandidateByState(name, state) {
+    return axios.get(`/${this.apiUrl}?estado=${state}&nome=${name}`);
+  }
+
+export default new Fetcher();
