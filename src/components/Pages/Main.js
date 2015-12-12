7dd3995de6d2e5fb8b8adcc768d7cd888c7ea1e2
resendefreitas <guilherme.freitas@vtex.com.br>
2015-12-12 05:01:12 -0200

    Ajustes less pagina inicial

--- src/components/Pages/Main.js
+++ src/components/Pages/Main.js
@@ -1,19 +1,24 @@
-require('normalize.css');
-require('styles/App.css');
-
 import React from 'react';
 import {Link} from 'react-router';
+
+import '../../styles/main.less';
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="CandidateScan component">
         <div className="camera">
-          <span>This is a space reserved to camera scan</span>
+          <br/><br/><br/><br/><br/><br/><br/>
+          <span className="col-xs-12 camera--text">This is a space reserved to camera scan</span>
         </div>
-        <div className="container">
-          <Link to="/test">
-            <button type="submit" className="btn btn-success btn-block enter">Lista de candidados</button>
+        <div className="instruction">
+          <Link to={'/list'}>
+            <span className="col-xs-12 instruction--text">
+              Aponte sua câmera para ter o reconhecimento facial automático do candidato
+            </span>
+            <button className="col-xs-offset-1 col-xs-10 btn gotolist">
+              <span className="instruction--text">Candidados</span>
+            </button>
           </Link>
         </div>
       </div>
