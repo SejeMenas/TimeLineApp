d8c3ff1185fdc8837f2e81ba693108e6cfec5dbf
resendefreitas <guilherme.freitas@vtex.com.br>
2015-12-12 10:51:10 -0200

    Cria rota e mocka pagina da timeline

---
+++ src/components/Pages/TimeLine.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CandidateItem from '../Candidate/Item/Item';
+
+export default class CandidateTimeLine extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <CandidateItem colorCycle={0}/>
+      </div>
+    );
+  }
+}
