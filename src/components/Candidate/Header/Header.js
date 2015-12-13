8d58fa1fc3a635726671a6d748e3b12475850bba
Luiza Breier <luizabreier21@gmail.com>
2015-12-12 23:34:22 -0200

    Faz novos ajustes do header

--- src/components/Candidate/Header/Header.js
+++ src/components/Candidate/Header/Header.js
@@ -4,11 +4,11 @@ import './Header.less';
 var CandidateHeader = React.createClass({
   render() {
     return (
-    <div className="container-fuid" id="principal">
+    <div className="container-fuid header" id="principal">
       <div className="row">
         <div className="col-xs-12">
-          <h3>O NOME DO POLITICO</h3>
-          <h3 className="pull-right partido">PSDB</h3>
+          <h3 className="text">NOME DO POLITICO</h3>
+          <h3 className="pull-right partido text">PSDB</h3>
             <div className="header-img">
               <div className="line-img">
               </div>
