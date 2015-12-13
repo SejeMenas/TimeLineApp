a5e99d5568c9112407b882f37d2349964ad20933
Luiza Breier <luizabreier21@gmail.com>
2015-12-12 23:35:00 -0200

    Faz ajustes na Timeline

--- src/components/Candidate/TimeLine/TimeLine.js
+++ src/components/Candidate/TimeLine/TimeLine.js
@@ -1,14 +1,33 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
 import './TimeLine.less';
 
-export default class CandidateHistory extends React.Component {
+export default class CandidateTimeLine extends React.Component {
 
   render() {
     return (
-      <div className="timeline--container">
-          <div className="main-title-border border"></div>
-          <br/><br/><br/>
-          <p>{this.props.candiData}</p>
+      <div className="timeline container">
+        <div className="header-img2">
+          <div className="header-img-detail-right first">
+            <div>
+            </div>
+            <div className="year-right">2015</div>
+          </div>
+          <div className="header-img2">
+            <div className="header-img-detail-left second">
+              <div className="year-left">2014</div>
+            </div>
+              <div className="header-img2">
+                <div className="header-img-detail-right third">
+                  <div className="year-right">2013</div>
+                  </div>
+                <div className="header-img2">
+                  <div className="header-img-detail-left fourth">
+                    <div className="year-left">2012</div>
+                  </div>
+                </div>
+                </div>
+              </div>
+          </div>
       </div>
     );
   }
